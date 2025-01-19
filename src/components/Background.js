e0a84b9aac6e5029dd3ffec4c48fb4032d823aa4
Paul Gallagher <129823808+PaulGallagher37@users.noreply.github.com>
2025-01-19 20:02:04 +0000

    update code for dynamic background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,22 +1,19 @@
 
-import background1 from "../images/background1.jpg"
-import background2 from "../images/background2.jpg"
-import background3 from "../images/background3.jpg"
-import background4 from "../images/background4.jpg"
-import background5 from "../images/background5.jpg"
-import background6 from "../images/background6.jpg"
 
-let backgrounds = [background1, background2, background3, background4, background5, background6];
-
-let randomNum = Math.floor(Math.random() * backgrounds.length)
 
 
 function Background() {
 
+    let backgrounds = ["one", "two", "three", "four"];
+
+    let randomNum = Math.floor(Math.random() * backgrounds.length);
+
+    let background = "background" + backgrounds[randomNum]
+
 
     return (
-        <div className="background">
-            <img src={backgrounds[randomNum]} width="100%" height="100%"></img>
+        <div>
+            <img className={background} width="100%" height="100%"></img>
         </div>
     )
 };
